663bfaafd51ad33f233078c5c5b6c46442dbfc9a
Shamblonaut <shaheembn@gmail.com>
2024-05-18 14:19:09 +0530

    Add mouse support to move and resize windows
    Also move instructions to a separate window at the bottom right corner

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,14 @@
 struct ActiveWindow {
   Window* window{nullptr};
   std::shared_ptr<Text> marker;
+
+  Position changePressPosition;
+
+  bool moving = false;
+  Position preChangePosition;
+
+  bool resizing = false;
+  Size preChangeSize;
 } activeWindow;
 
 void changeActiveWindow(Window* next);
@@ -39,6 +47,7 @@ int main() {
   mousemask(ALL_MOUSE_EVENTS | REPORT_MOUSE_POSITION, NULL);
   mouseinterval(166);
   MEVENT mouseEvent;
+  printf("\033[?1003h\n");  // Makes the terminal report mouse movement
 
   refresh();
 
@@ -47,10 +56,6 @@ int main() {
   getmaxyx(stdscr, screenHeight, screenWidth);
   /* mvprintw(0, 0, "Height: %d, Width: %d", screenHeight, screenWidth); */
   /* mvprintw(0, 0, "Maximum Colors: %d", COLORS); */
-
-  mvprintw(LINES - 3, 0, "Use WASD to move windows");
-  mvprintw(LINES - 2, 0, "Use Arrow Keys to resize windows");
-  mvprintw(LINES - 1, 0, "Use HJKL to change focus");
 
   // Main section
   int y = 5, x = 5, height = screenHeight / 4, width = screenWidth / 4;
@@ -92,6 +97,30 @@ int main() {
   windows[1][0].addText("Flat borders", MIDDLE_CENTER);
   windows[1][1].addText("Double borders", MIDDLE_CENTER);
   windows[1][2].addText("Rounded borders", MIDDLE_CENTER);
+
+  Window infoBox = Window(
+      (Position){screenHeight - 7, 0}, (Size){7, screenWidth / 4 + 10},
+      Title("Instructions", LEFT, BLUE, 0), Border::ROUNDED, CYAN
+  );
+  infoBox.addText(
+      "- Use WASD to move windows", TOP_LEFT, GREEN, (Offset){0, 0}
+  );
+  infoBox.addText(
+      "- Use Arrow Keys to resize windows", TOP_LEFT, GREEN, (Offset){1, 0}
+  );
+  infoBox.addText(
+      "- Use HJKL to change focus", TOP_LEFT, GREEN, (Offset){2, 0}
+  );
+  infoBox.addText(
+      "- Click on a window to focus", TOP_LEFT, GREEN, (Offset){3, 0}
+  );
+  infoBox.addText(
+      "- Click and drag on window to move it", TOP_LEFT, GREEN, (Offset){4, 0}
+  );
+  infoBox.addText(
+      "- Click and drag the right/bottom border to resize window", TOP_LEFT,
+      GREEN, (Offset){4, 0}
+  );
 
   int winI = 0, winJ = 1;
   changeActiveWindow(&windows[winI][winJ]);
@@ -152,26 +181,67 @@ int main() {
       changeActiveWindow(&windows[winI][winJ]);
     }
 
+    infoBox.render();
+
     bool mouseClicked = ch == KEY_MOUSE && getmouse(&mouseEvent) == OK;
-    /*if (mouseClicked) {*/
-    /*  mvprintw(*/
-    /*      0, 0, "Mouse event; X: %d, Y: %d, ID: %08x, Pressed: %08x",*/
-    /*      mouseEvent.x, mouseEvent.y, mouseEvent.bstate, BUTTON1_TRIPLE_CLICKED*/
-    /*  );*/
-    /*}*/
 
     for (int i = 0; i < 2; i++) {
       for (int j = 0; j < 3; j++) {
-        if (mouseClicked && mouseEvent.bstate == BUTTON1_CLICKED &&
-            wenclose(windows[i][j].win, mouseEvent.y, mouseEvent.x)) {
-          changeActiveWindow(&windows[i][j]);
+        if (mouseClicked) {
+          if (wenclose(windows[i][j].win, mouseEvent.y, mouseEvent.x)) {
+            if (!activeWindow.moving) changeActiveWindow(&windows[i][j]);
+            if (mouseEvent.bstate == BUTTON1_PRESSED) {
+              if (mouseEvent.y == activeWindow.window->position.y +
+                                      activeWindow.window->size.height - 1 ||
+                  mouseEvent.x == activeWindow.window->position.x +
+                                      activeWindow.window->size.width - 1) {
+                activeWindow.resizing = true;
+                activeWindow.preChangeSize = activeWindow.window->size;
+              } else {
+                activeWindow.moving = true;
+                activeWindow.preChangePosition = activeWindow.window->position;
+              }
+
+              activeWindow.changePressPosition =
+                  (Position){mouseEvent.y, mouseEvent.x};
+
+              activeWindow.window->winBorder.setColor(MAGENTA, false);
+            }
+          }
+          if (mouseEvent.bstate == REPORT_MOUSE_POSITION) {
+            if (activeWindow.moving) {
+              activeWindow.window->move((Position
+              ){activeWindow.preChangePosition.y +
+                    (mouseEvent.y - activeWindow.changePressPosition.y),
+                activeWindow.preChangePosition.x +
+                    (mouseEvent.x - activeWindow.changePressPosition.x)});
+            } else if (activeWindow.resizing) {
+              activeWindow.window->resize((Size
+              ){activeWindow.preChangeSize.height +
+                    (mouseEvent.y - activeWindow.changePressPosition.y),
+                activeWindow.preChangeSize.width +
+                    (mouseEvent.x - activeWindow.changePressPosition.x)});
+            }
+            activeWindow.window->winBorder.setColor(MAGENTA, false);
+          } else if (mouseEvent.bstate == BUTTON1_RELEASED) {
+            if (activeWindow.moving) {
+              activeWindow.moving = false;
+            } else if (activeWindow.resizing) {
+              activeWindow.resizing = false;
+            }
+
+            activeWindow.window->winBorder.setColor(GREEN, false);
+          }
         }
+
         windows[i][j].render();
       }
     }
 
     ch = getch();
   }
+
+  printf("\033[?1003h\n");  // Disable mouse movement report
 
   // Close curses mode
   endwin();
